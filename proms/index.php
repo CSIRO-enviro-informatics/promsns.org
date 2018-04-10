38600b4dace3d44b4f7f1ae8f717fe2db59102b3
Nicholas Car <nicholas.car@ga.gov.au>
2018-04-10 10:41:28 +1000

    updates PROMS page

--- proms/index.php
+++ proms/index.php
@@ -4,10 +4,41 @@
     include $root_path . 'theme/header.inc';
 ?>
 
-<h1>PROMS: PROVenance Management System</h1>
-<p><em>This page will soon be the new homepage for the PROMS family of tools. For the moment, please see the PROMs wiki:</em></p>
+<h1>PROVenance Management System</h1>
+<p><em>This page is the new homepage for the PROMS family of tools. Currently, the way PROMS Server works and much background context are given in the PROMS wiki:</em></p>
 <ul>
     <li><a href="https://promsns.org/wiki/proms">All about PROMS - the PROMS wiki</a></li>
 </ul>
+
+<h2>PROMS instances</h2>
+<p>Here are some publicly visible instances of PROMS:</p>
+<table>
+    <tr>
+        <td><a href="https://research.csiro.au/digiscape/"><img src="../theme/img/logo.png" title="CSIRO logo" style="width:100px;"/></a></td>
+        <td style="width:50px;">&nbsp;</td>
+        <td><a href="http://18.188.14.133/" style="vertical-align:middle;">DigiScape Future Science Platform project - dev instance</a></td>
+    </tr>
+    <tr>
+        <td><a href="http://eudm.csiro.au"><img src="EUDM-logo.png" title="EUDM logo" style="width:100px;"/></a></td>
+        <td style="width:50px;">&nbsp;</td>
+        <td><a href="http://52.64.5.57/proms/">Energy Use Data Model project - dev instance</a></td>
+    </tr>
+</table>
+
+<h2>Additional Tools</h2>
+<p>Here are some additional tools that have been developed to assist with the use of PROMS:</p>
+<table>
+    <tr>
+        <tr>
+            <td><a href="https://promsns.org//repo/proms-reporter-python">PROMS Reporter Python</a> - a Python client for reporting to PROMS</td>
+        </tr>
+        <tr>
+            <td><a href="https://promsns.org//repo/proms-reporter-java">PROMS Reporter Java</a> - a Java client for reporting to PROMS</td>
+        </tr>
+        <tr>
+            <td><a href="https://promsns.org//repo/proms-reporter-dotnet">PROMS Reporter .NET</a> - a .NET client for reporting to PROMS</td>
+        </tr>
+    </tr>
+</table>
 
 <?php include $root_path . 'theme/footer.inc'; ?>
